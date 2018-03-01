115ee888d03bd96c92afd7e6d9a3ac96589f149e
allazis <charlotta.jaunviksna@gmail.com>
2018-03-01 15:07:14 +0100

    Implemented loop unrolling and strength reduction modification in B_fun

--- utils.c
+++ utils.c
@@ -10,14 +10,26 @@ double get_sq_contrib(double x, int d) {
 int L = 20;
 
 double B_fun(int B_q) {
-  double x = (double)B_q / 1000;
+  double x = (double)B_q * 0.001;
   double sum = 0;
   double product = 1;
   int i;
-  for(i = 0; i < L; i++) {
+  for(i = 0; i < L; i+= 5) {
     product *= (x+i*0.007);
     //if(product >= 0)
-      sum += product;
+    sum += product;
+
+    product *= (x+(i+1)*0.007);
+    sum += product;
+
+    product *= (x+(i+2)*0.007);
+    sum += product;
+
+    product *= (x+(i+3)*0.007);
+    sum += product;
+
+    product *= (x+(i+4)*0.007);
+    sum += product;
   }
   return sum;
 }
