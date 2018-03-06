3bdf9dafc922083602a67628af22b8686edacea2
allazis <charlotta.jaunviksna@gmail.com>
2018-03-06 13:45:26 +0100

    Created calc_new_state function

--- timestep.c
+++ timestep.c
@@ -3,13 +3,8 @@
 #include "structs.h"
 #include "funcs.h"
 
-int do_one_time_step(int N, cell_t* cells) {
-
-  /* Allocate memory for the result after the timestep. */
-  int n_cells = N*N;
-  cell_t* cells2 = (cell_t*)malloc(n_cells*sizeof(cell_t));
-
-  /* Copy the part of the data for each cell that is supposed to remain the same. */
+void calc_new_state(cell_t * cells, cell_t * cells2, int N, int n_cells) {
+	  /* Copy the part of the data for each cell that is supposed to remain the same. */
   int i, j;
   for(i = 0; i < n_cells; i++) {
     cells2[i].A_g = cells[i].A_g;
@@ -41,112 +36,6 @@ int do_one_time_step(int N, cell_t* cells) {
     }
   } 
 
-
-  // for(i = 1; i < N-1; i++) {
-  //   for(j = 1; j < N-1; j++) {
-  //     int idx = i+j*N;
-  //     /* Find A-values from neighboring cells. */
-  //     double neighbor_A_sum = 0;
-  //     /* neighbor i-1 */
-  //     neighbor_A_sum += cells[(i-1)+j*N].A;
-  //     /* neighbor i+1 */
-  //     neighbor_A_sum += cells[(i+1)+j*N].A;
-  //     /* neighbor j-1 */
-  //     neighbor_A_sum += cells[i+(j-1)*N].A;
-  //     /* neighbor j+1 */
-  //     neighbor_A_sum += cells[i+(j+1)*N].A;
-  //     neighbor_A_sums[idx] = neighbor_A_sum;
-  //   }
-  // }
-
-  // /* Top and bottom borders */
-  // for (i = 1; i < N-1; i++) {
-  // 	int j = 0;
-  // 	int idx = i+j*N;
-  // 	double neighbor_A_sum = 0;
-  //   /* neighbor i-1 */
-  //   neighbor_A_sum += cells[(i-1)+j*N].A;
-  //   /* neighbor i+1 */
-  //   neighbor_A_sum += cells[(i+1)+j*N].A;
-  //   /* neighbor j+1 */
-  //   neighbor_A_sum += cells[i+(j+1)*N].A;
-  //   neighbor_A_sums[idx] = neighbor_A_sum;
-
-  //   j = N;
-  // 	idx = i+j*N;
-  // 	neighbor_A_sum = 0;
-
-  // 	/* neighbor i-1 */
-  //   neighbor_A_sum += cells[(i-1)+j*N].A;
-  //   /* neighbor i+1 */
-  //   neighbor_A_sum += cells[(i+1)+j*N].A;
-  //   /* neighbor j-1 */
-  //   neighbor_A_sum += cells[i+(j-1)*N].A;
-  //   neighbor_A_sums[idx] = neighbor_A_sum;
-  // }
-
-  // /* Left and right borders */
-  // for (j = 1; j < N-1; i++) {
-  // 	int i = 0;
-  // 	int idx = i+j*N;
-  // 	double neighbor_A_sum = 0;
-  // 	/* neighbor i+1 */
-  //   neighbor_A_sum += cells[(i+1)+j*N].A;
-  //   /* neighbor j-1 */
-  //   neighbor_A_sum += cells[i+(j-1)*N].A;
-  //   /* neighbor j+1 */
-  //   neighbor_A_sum += cells[i+(j+1)*N].A;
-  //   neighbor_A_sums[idx] = neighbor_A_sum;
-
-  //   i = N;
-  // 	idx = i+j*N;
-  // 	neighbor_A_sum = 0;
-  // 	/* neighbor i-1 */
-  //   neighbor_A_sum += cells[(i-1)+j*N].A;
-  //   /* neighbor j-1 */
-  //   neighbor_A_sum += cells[i+(j-1)*N].A;
-  //   /* neighbor j+1 */
-  //   neighbor_A_sum += cells[i+(j+1)*N].A;
-  //   neighbor_A_sums[idx] = neighbor_A_sum;
-  // }
-
-  // /* Corners */
-  // int idx = 0+0*N;
-  // neighbor_A_sums[idx] = cells[(i+1)+j*N].A + cells[i+(j+1)*N].A;
-  // idx = 0+N*N;
-  // neighbor_A_sums[idx] = cells[(i-1)+j*N].A + cells[i+(j+1)*N].A;
-  // idx = N+0*N;
-  // neighbor_A_sums[idx] = cells[(i+1)+j*N].A + cells[i+(j-1)*N].A;
-  // idx = N+N*N;
-  // neighbor_A_sums[idx] = cells[(i-1)+j*N].A + cells[i+(j-1)*N].A;
-  
-
-  // /* Loop fusion test */
-  //   double A, neighbor_A_sum, B, C;
-  // int A_g, B_q, C_h, C_i;
-  // for(i = 0; i < n_cells; i++) {
-  //   A = cells[i].A;
-  //   B = cells[i].B;
-  //   C = cells[i].C;
-
-  //   /* Compute new value of A for each cell. */
-  //   A_g = cells[i].A_g;
-  //   neighbor_A_sum = neighbor_A_sums[i];
-  //   cells2[i].A = compute_new_A(A_g, A, B, C, neighbor_A_sum);
-
-  //   /* Compute new value of B for each cell.  */
-  //   B_q = cells[i].B_q;
-  //   cells2[i].B = compute_new_B(B_q, A, B, C);
-
-  // /* Compute new value of C for each cell. */
-  //   C_h = cells[i].C_h;
-  //   C_i = cells[i].C_i;
-  //   cells2[i].C = compute_new_C(C_h, C_i, A, B, C);
-   
-  //   /* Copy final result back to cells array. */
-  //   cells[i] = cells2[i];
-  // }
-
   /* Compute new value of A for each cell. */
   for(i = 0; i < n_cells; i++) {
     int A_g = cells[i].A_g;
@@ -156,6 +45,7 @@ int do_one_time_step(int N, cell_t* cells) {
     double neighbor_A_sum = neighbor_A_sums[i];
     cells2[i].A = compute_new_A(A_g, A, B, C, neighbor_A_sum);
   }
+  free(neighbor_A_sums);
 
   /* Compute new value of B for each cell. */
   for(i = 0; i < n_cells; i++) {
@@ -175,15 +65,24 @@ int do_one_time_step(int N, cell_t* cells) {
     double C = cells[i].C;
     cells2[i].C = compute_new_C(C_h, C_i, A, B, C);
   }
+}
+
+
+int do_one_time_step(int N, cell_t* cells) {
+
+  /* Allocate memory for the result after the timestep. */
+  int n_cells = N*N;
+  cell_t* cells2 = (cell_t*)malloc(n_cells*sizeof(cell_t));
+
+  calc_new_state(cells, cells2, N, n_cells);
 
   /* Copy final result back to cells array. */
-  for(i = 0; i < n_cells; i++) {
+  for(int i = 0; i < n_cells; i++) {
     cells[i] = cells2[i];
   }
 
   /* Free memory. */
   free(cells2);
-  free(neighbor_A_sums);
 
   /* Return 0 to indicate that the function finished OK. */
   return 0;
--- timestep.h
+++ timestep.h
@@ -1,3 +1,5 @@
 #include "structs.h"
 
 int do_one_time_step(int N, cell_t* cells);
+
+void calc_new_state(cell_t * cells, cell_t * cells2, int N, int n_cells);
